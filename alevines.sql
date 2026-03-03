57f7eeb20df714de333889b259aa80e0ba60fd01
Fernando <fernandoemiliolopez13@gmail.com>
2026-03-02 19:05:06 -0600

    Add coding style and commit style guidelines

--- alevines.sql
+++ alevines.sql
@@ -2487,38 +2487,11 @@ CREATE TABLE catalogos.estados (
 );
 
 INSERT INTO catalogos.estados (fc_nombre) VALUES
-('Aguascalientes'),
-('Baja California'),
-('Baja California Sur'),
-('Campeche'),
-('Chiapas'),
-('Chihuahua'),
-('Ciudad de México'),
-('Coahuila'),
-('Colima'),
-('Durango'),
-('Estado de México'),
-('Guanajuato'),
-('Guerrero'),
-('Hidalgo'),
-('Jalisco'),
-('Michoacán'),
-('Morelos'),
-('Nayarit'),
-('Nuevo León'),
-('Oaxaca'),
-('Puebla'),
-('Querétaro'),
-('Quintana Roo'),
-('San Luis Potosí'),
-('Sinaloa'),
-('Sonora'),
-('Tabasco'),
-('Tamaulipas'),
-('Tlaxcala'),
-('Veracruz'),
-('Yucatán'),
-('Zacatecas');
+('Activo'),
+('Inactivo'),
+('Vacaciones'),
+('Baja Temporal'),
+('Baja Definitiva');
 
 CREATE SCHEMA IF NOT EXISTS rrhh;
 
